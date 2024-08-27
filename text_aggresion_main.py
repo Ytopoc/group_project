297299e4a68545bd67f4c64325375bbf81febfb3
kostyantynLS <158769336+kostyantynLS@users.noreply.github.com>
2024-08-28 01:46:24 +0300

    Add files via upload

--- text_aggresion_main.py
+++ text_aggresion_main.py
@@ -8,16 +8,16 @@ sources_remote = (
             'https://docs.google.com/spreadsheets/d/1I-WycB-n8VvkKrmEvuvA4n-ctcMYyH9o58T783Byt2k/export?format=csv',
             'https://docs.google.com/spreadsheets/d/17J2k4Cz8FwTGEyQ2mwLLKbFgqbgK9D2a5zfQiuJBt6M/export?format=csv')
 
-# def LoadData(source)
-# завантаження даних у DataFrame
+#   def LoadData(source)
+#   завантаження даних у DataFrame
 import text_load
 
-# def clean_pd_data(pd_data, column_name)
-# приведення рядків у нижній формат та видалення знаків пунктуації
-# def tokenize_and_encode(tokenizer, comments, labels, max_length=128)
+#   def clean_pd_data(pd_data, column_name)
+#   приведення рядків у нижній формат та видалення знаків пунктуації
+#   def tokenize_and_encode(tokenizer, comments, labels, max_length=128)
 import text_prepare
 
-# import text_analysis
+#import text_tokenize
 
 df_train = text_load.LoadData(source=sources_local[0])
 print('clearing train')
@@ -26,4 +26,3 @@ print('clearing train')
 df_test = text_load.LoadData(source=sources_local[1])
 print('clearing test')
 df_test = text_prepare.clean_pd_data(df_test, 'comment_text')
-
