d59ac63f1ea5849ae849d549da6536139b3a9b69
kostyantynLS <158769336+kostyantynLS@users.noreply.github.com>
2024-08-28 01:04:29 +0300

    Add files via upload

--- text_aggresion_main.py
+++ text_aggresion_main.py
@@ -3,13 +3,31 @@ import numpy as np
 import pandas as pd
 
 # 
+sources_local = ('train - train.csv', 'test - test.csv')
+sources_remote = (
+            'https://docs.google.com/spreadsheets/d/1I-WycB-n8VvkKrmEvuvA4n-ctcMYyH9o58T783Byt2k/export?format=csv',
+            'https://docs.google.com/spreadsheets/d/17J2k4Cz8FwTGEyQ2mwLLKbFgqbgK9D2a5zfQiuJBt6M/export?format=csv')
+
+# def LoadData(source)
+# завантаження даних у DataFrame
 import text_load
+
+# def clean_pd_data(pd_data, column_name)
+# приведення рядків у нижній формат та видалення знаків пунктуації
+# def tokenize_and_encode(tokenizer, comments, labels, max_length=128)
 import text_prepare
 
-df_train = text_load.LoadData(source="train - train.csv")
+# import text_analysis
+
+df_train = text_load.LoadData(source=sources_local[0])
 print('clearing train')
 df_train = text_prepare.clean_pd_data(df_train, 'comment_text')
 
-df_test = text_load.LoadData(source="test - test.csv")
+print(df_train['comment_text'][0])
+print(df_train['comment_text'][10])
+
+
+df_test = text_load.LoadData(source=sources_local[1])
 print('clearing test')
 df_test = text_prepare.clean_pd_data(df_test, 'comment_text')
+
--- text_load.py
+++ text_load.py
@@ -1,5 +1,3 @@
-#import numpy as np
-
 import pandas as pd
 
 #to avoid warnings
@@ -10,22 +8,3 @@ warnings.filterwarnings('ignore')
 def LoadData(source = "my.csv"):
     pd_data = pd.read_csv(source)
     return pd_data
-
-def LoadTrainData(source = 'local'):
-    print('train data', end=" ")
-    if source == 'local':
-        pd_data = pd.read_csv('train - train.csv')
-    else:
-        pd_data = pd.read_csv('https://docs.google.com/spreadsheets/d/1I-WycB-n8VvkKrmEvuvA4n-ctcMYyH9o58T783Byt2k/export?format=csv')
-    print(', done')
-    return pd_data
-
-def LoadTestData(source = 'local'):
-    print('test data', end=" ")
-    if source == 'local':
-        pd_data = pd.read_csv('test - test.csv')
-    else:
-        pd_data = pd.read_csv('https://docs.google.com/spreadsheets/d/17J2k4Cz8FwTGEyQ2mwLLKbFgqbgK9D2a5zfQiuJBt6M/export?format=csv')
-    print(', done')
-    return pd_data
-
